8c7696a944784c49e1c20fdc71bc671eecd35bb3
admin <admin@Waseem-Akram-Mac.local>
2018-11-13 11:15:27 +0530

    Initial Commit

---
+++ playerPodDemo/playerPodDemo.h
@@ -0,0 +1,19 @@
+//
+//  playerPodDemo.h
+//  playerPodDemo
+//
+//  Created by Waseem Akram on 13/11/18.
+//  Copyright © 2018 Zee  Enterprises Entertainment Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for playerPodDemo.
+FOUNDATION_EXPORT double playerPodDemoVersionNumber;
+
+//! Project version string for playerPodDemo.
+FOUNDATION_EXPORT const unsigned char playerPodDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <playerPodDemo/PublicHeader.h>
+
+
